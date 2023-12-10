5548a3313c85180198123bef47f0a8556dd716d0
cmackethan <cmackethan@vt.edu>
2023-12-10 16:43:15 -0500

    Cleaning up

---
+++ cleanup.py
@@ -0,0 +1,12 @@
+import os
+
+cwd = os.getcwd()
+ls = os.listdir(cwd)
+ls.remove('.git')
+
+for fname in ls:
+    fpath = os.path.join(cwd, fname)
+    if os.path.isdir(fpath):
+        print(fpath + '/input.txt')
+        os.system('git rm --cached ' + fpath + '/input.txt')
+        os.system('git rm --cached ' + fpath + '/testinput.txt')
